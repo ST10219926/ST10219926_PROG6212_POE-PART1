32a88b099ef49026de3fcf0d8163e526bd11b828
ST10219926 <162098002+ST10219926@users.noreply.github.com>
2025-11-07 12:39:37 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using CMCS.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection") ?? "Data Source=claims.db"));
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Account/Login";
+    });
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+app.Run();
---
+++ RUN.md
@@ -0,0 +1,7 @@
+Quick run instructions (inside ContractMonthlyClaimSystem folder):
+
+1. dotnet restore
+2. dotnet tool install --global dotnet-ef   (if you don't have dotnet-ef)
+3. dotnet ef migrations add Initial
+4. dotnet ef database update
+5. dotnet run
